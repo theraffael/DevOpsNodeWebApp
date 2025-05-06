fef2e8c421dca68ed5a641b4f08d449eb764b6b7
raffael <raffael.kummer@uzh.ch>
2025-05-06 15:35:00 +0200

    changed localhost

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 // Environment variables
 const PORT = process.env.PORT || 3001;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || 3000;
 
 // App setup
 const app = express();
