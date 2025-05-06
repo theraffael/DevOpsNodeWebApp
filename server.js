ef02d68f0d14c634171bb41d3636c7040c4a8f96
raffael <raffael.kummer@uzh.ch>
2025-05-06 15:45:39 +0200

    copied server.js from mosazhaw

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 'use strict';
 
-// Import dependencies
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-require('dotenv').config();
 
-// Environment variables
-const PORT = process.env.PORT || 3001;
-const HOST = process.env.HOST || '0.0.0.0';
+// Constants
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 
-// App setup
+// App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Es ist Dienstagnachmittag :-) Cloud Deploy auf Render.com - 2nd attempt');
+  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Render.com; 3rd attempt');
 });
 
 app.listen(PORT, HOST);
